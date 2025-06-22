f0cd623c3870a3eab0c3ce9aa6845e0a53af6bd0
Aditya <adityapawar826@gmail.com>
2025-06-22 14:20:15 +0530

    custom validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,34 +4,39 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular-form';
 
-  @ViewChild('registrationForm')form:NgForm
+  @ViewChild('registrationForm') form: NgForm;
 
-  genders=[{
-    id:'check-male',
-    value:'Male',
-    display:'Male'
-  },
-{
-  id:'check-female',
-  value:'Female',
-  display:'Female'
-},
-{
-  id:'other',
-  value:'other',
-  display:'prefer Not to say'
-}]
+  genders = [
+    {
+      id: 'check-male',
+      value: 'Male',
+      display: 'Male',
+    },
+    {
+      id: 'check-female',
+      value: 'Female',
+      display: 'Female',
+    },
+    {
+      id: 'other',
+      value: 'other',
+      display: 'prefer Not to say',
+    },
+  ];
 
-  onFormSubmitted(){
-    console.log( this.form)
-    console.log( this.form.controls['firstname'].value)
-    console.log( this.form.value.lastname)
-    console.log( this.form.value.email)
+  firstName='';
+  lastName='';
+  emailAddress='';
 
+  onFormSubmitted() {
+    console.log(this.form);
+    console.log(this.form.controls['firstname'].value);
+    console.log(this.form.value.lastname);
+    console.log(this.form.value.email);
   }
 }
