87787a39dc51a10754465713b070b14637a142d2
Aditya <adityapawar826@gmail.com>
2025-06-22 14:45:38 +0530

    Grouping Form controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,12 +31,14 @@ export class AppComponent {
 
   firstName='';
   lastName='';
-  emailAddress='';
+  emailAddress=''
 
   onFormSubmitted() {
     console.log(this.form);
     console.log(this.form.controls['firstname'].value);
     console.log(this.form.value.lastname);
     console.log(this.form.value.email);
+    console.log(this.form.value.address.city)
+        console.log(this.form.value.address.country)
   }
 }
