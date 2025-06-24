7cd8ab4228716d652828b620b197944afb081d28
Aditya <adityapawar826@gmail.com>
2025-06-24 21:17:39 +0530

    SetValue method example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,9 +29,10 @@ export class AppComponent {
     },
   ];
 
-  firstName='';
-  lastName='';
-  emailAddress=''
+  firstName:string='';
+  lastName:string='';
+  emailAddress:string='';
+  dob:string=''
 
   onFormSubmitted() {
     console.log(this.form);
@@ -41,4 +42,45 @@ export class AppComponent {
     console.log(this.form.value.address.city)
         console.log(this.form.value.address.country)
   }
+
+  createUsername(){
+    let username='';
+    if(this.firstName.length>=3){
+      username+=this.firstName.slice(0,3);
+    }
+    else{
+      username+=this.firstName;
+    }
+     if(this.lastName.length>=3){
+      username+=this.lastName.slice(0,3);
+    }
+    else{
+      username+=this.lastName;
+    }
+
+    let dateTime=new Date(this.dob);
+      username+=dateTime.getFullYear();
+
+      username+=username.toLowerCase();
+      console.log(username);
+      this.form.setValue({
+      firstname:this.form.value.firstname,
+      lastname:this.form.value.lastname,
+      email:this.form.value.email,
+      phonenumber:this.form.value.phonenumber,
+      dateofbirth:this.form.value.dateofbirth,
+      username:username,
+        gender:this.form.value.gender,
+        address:{
+          street2:this.form.value.address.street2,
+          street1:this.form.value.address.street1,
+          country:this.form.value.address.country,
+          city:this.form.value.address.city,
+          region:this.form.value.address.region,
+          postal:this.form.value.address.postal
+
+        }
+      })
+  }
+  
 }
